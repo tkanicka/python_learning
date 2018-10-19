5d3c65adec30aeaaee8e6199e66fa5b1ad9c92f3
tkanicka <44132671+tkanicka@users.noreply.github.com>
2018-10-19 21:07:53 +0200

    Add files via upload

---
+++ homework/prime_numbers_improved.py
@@ -0,0 +1,12 @@
+
+def is_prime(x):
+    if x%2 ==0:
+        return False
+    for y in range(3,int(x**0.5)+1,2):
+        if x%y==0:
+            return False
+        else:
+            continue
+    return True
+x= int(input("give me an integer: "))
+print(is_prime(x))
