48a7ff4d89a5b135368fabaf1d46f53f709dd559
tkanicka <Slonik7bob@gmail.com>
2018-11-16 14:29:06 +0100

    improvements

--- homework(november)/homeworkAssigment2/greatest_common _divisor.py
+++ homework(november)/homeworkAssigment2/greatest_common _divisor.py
@@ -8,16 +8,12 @@ def GCD(x,y):
     return gcd
 
 def GCD_recursion(x,y):
-    if x == 0:
-        return y                        # example 0 % 5; 5%5 = 0
-    elif y == 0:
-        return x
 
     higher_number = max(abs(x), abs(y))
     smaller_number = min(abs(x), abs(y))
 
     if higher_number % smaller_number == 0:
-        return smaller_number                   # example(15;9): 15%9 = 6 -> 9%6 = 3 => 15,9,6,3 % 3 = 0
+        return smaller_number                   # example(15;9): 15%9 = 6 -> 9%6 = 3 -> 6%3 = 0 => 15,9,6,3 % 3 = 0
 
     else:
         return GCD_recursion(smaller_number, higher_number % smaller_number)
