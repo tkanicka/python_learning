b433619f6d2a679d874b20b7e61aa3b4fda20102
tkanicka <Slonik7bob@gmail.com>
2018-11-22 20:45:44 +0100

    improved and corrected

--- homework(november)/homeworkAssigment2/circle.py
+++ homework(november)/homeworkAssigment2/circle.py
@@ -1,11 +1,11 @@
 def print_circle(r):
     d = 2*r
 
-    for horizontal in range(d+1):
-        for vertical in range(d+1):
-            circuit_distance = ((horizontal - r)**2 + (vertical - r)**2)**0.5           # distance  will be +/- some number
+    for column in range(d+1):
+        for row in range(d+1):
+            circuit_distance = ((column - r)**2 + (row - r)**2)**0.5          
 
-            if (circuit_distance > r - 1 and circuit_distance < r + 1):  # making a tolerance
+            if (circuit_distance > r - 0.5 and circuit_distance < r + 0.5):  # making a tolerance so the distance is +- r
                 print("*", end = " ")
             else:
                 print(" ", end = " ")
