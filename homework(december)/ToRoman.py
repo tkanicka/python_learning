23dab81c338bd6c3e2d49469ef3d7fe4c06be74e
tkanicka <Slonik7bob@gmail.com>
2018-12-07 16:21:37 +0100

    probably the most complicated way for creating a chessboard using a 2D field...

--- homework(december)/ToRoman.py
+++ homework(december)/ToRoman.py
@@ -4,5 +4,7 @@ def ToRoman(number):
     elif number > 3999:
         raise ValueError("Your input must not exceed the value of 3000 ")
 
-    Numerals = [(1, "I"), (4, "IV"), (5, "V"), (9, "IX"), (10, "X"), (40, "XL"), (90, "XC"), (100, "C"), (400, "CD"),
+    numerals = [(1, "I"), (4, "IV"), (5, "V"), (9, "IX"), (10, "X"), (40, "XL"), (90, "XC"), (100, "C"), (400, "CD"),
                 (500, "D"), (900, "CM"), (1000, "M")]
+
+print(ToRoman(10000))
