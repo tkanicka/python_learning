4dcfdb4cda23ca5dca9dbb9a44318b4c0b2c4007
tkanicka <Slonik7bob@gmail.com>
2019-05-08 21:18:03 +0200

    added checks for the users inputs, removed automatic printing, and other cosmetic details :)

--- knapsack_problem/state_generator.py
+++ knapsack_problem/state_generator.py
@@ -1,68 +1,101 @@
 
 class StateGenerator:
 
-    def __init__(self, index, taken, value, weight, prices, weights ):
+    def __init__(self, index, taken, prices, weights ):
 
-        self.value = value
-        self.weight = weight
-        self.taken = taken
-        self.index = index
-        self.prices = prices
-        self.weights = weights
+        if len(prices) == len(weights):         # checking the entries
+            self.prices = prices
+            self.weights = weights
 
-    def get_NextState(self):
+            if index <= len(prices):
+                self.index = index
+
+            else:
+                raise ValueError("your index is out of range")
+
+            if len(taken) == len(prices):
+                self.taken = taken
+
+            else:
+                raise ValueError("your list of taken items must have the same length as your list of items")
+
+                self.value = 0
+                self.weight = 0
+                for i in range(len(self.taken)):        # calculating value and weight according to your taken items
+                    if self.taken[i] == 1:
+                        self.value += self.prices[i]
+                        self.weight += self.weights[i]
+
+        else:
+            raise ValueError("lists of values and prices must have the same length")
+
+    def get_NextState(self, print_taken = False):
+
         if self.index < len(self.prices) and self.taken[self.index] != 1:
             new_taken = list(self.taken)
             new_taken[self.index] = 1
 
-            new_state = StateGenerator(self.index +1, new_taken, self.value + self.prices[self.index],
-                                       self.weight + self.weights[self.index], self.prices, self.weights)
-            print(new_state.taken)
+            new_state = StateGenerator(self.index +1, new_taken,
+                                       self.prices, self.weights)
+            if print_taken:
+                print(new_state.taken)
+
             return new_state
 
         elif self.index < len(self.prices) and self.taken[self.index] == 1:
-            new_state = StateGenerator(self.index + 1, self.taken, self.value,      # if current item is taken, just increase your index
-                                       self.weight, self.prices, self.weights)      # and try to generate next state
-            return new_state.get_NextState()
+            new_state = StateGenerator(self.index + 1, self.taken,       # if current item is taken, just increase your index
+                                       self.prices, self.weights)      # and try to generate next state
+            return new_state.get_NextState(print_taken)
 
         else:
             return None
 
-    def get_allNextStates(self):
 
-        while self.index < len(self.prices):        # goes over all possible states that can be generated from your current state
-            self.get_NextState()
+    def get_allNextStates(self,print_taken = False):
+        i = self.index
+        while self.get_NextState():        # goes over all possible states that can be generated from your current state
+           self.get_NextState(print_taken)
+           self.index += 1
+
+        self.index = i
+
+    def get_AllPotentialStates(self, print_taken = False):    # goes through all possible combinations from your items that can be made further from your current state
+        i = self.index                                       # develops the branch you are in. It is done in DFS order
+        while self.index < len(self.prices):
+
+            self.get_NextState(print_taken).get_AllPotentialStates(print_taken)
             self.index += 1
-
-    def get_AllPotentialStates(self):    # goes through all possible combinations from your items that can be made further from your current state
-                                         # develops the branch you are in. It is done in DFS order
-            while self.index < len(self.prices):
-                self.get_NextState().get_AllPotentialStates()
-                self.index += 1
+        self.index = i
 
 
 if __name__ == "__main__":
+
     prices = [5, 10, 25]
     weights = [1, 2, 3]
 
-    empty_state = StateGenerator(0, [0] * len(prices), 0, 0, prices, weights)
-    end_state = StateGenerator(2, [0, 0, 1], 25, 3, prices, weights)
+    empty_state = StateGenerator(0, [0] * len(prices), prices, weights)
+    end_state = StateGenerator(2, [0, 0, 1], prices, weights)
 
+    print(end_state.value, end_state.weight)
+
+    empty_state.get_AllPotentialStates(True)
+
+    print("\n")
+
+    print(empty_state.get_NextState().value)
+
+    empty_state.get_NextState().get_allNextStates(True)
+
+    print("\n")
+
+    empty_state.get_allNextStates(True)
+
+    print("\n")
+    empty_state.index = 1
     empty_state.get_AllPotentialStates()
 
     print("\n")
     empty_state.index = 0
-    empty_state.get_NextState().get_allNextStates()
+    empty_state.get_NextState()
 
-    print("\n")
-    empty_state.index = 1
-    empty_state.get_allNextStates()
-
-    print("\n")
-    empty_state.index = 1
-    empty_state.get_AllPotentialStates()
-
-    print("\n")
-    empty_state.index = 0
-    empty_state.get_allNextStates()
 
--- knapsack_problem/test_stateGenerator.py
+++ knapsack_problem/test_stateGenerator.py
@@ -7,9 +7,9 @@ class TestStateGenerator(unittest.TestCase):
         prices = [5, 10, 25]
         weights = [1, 2, 3]
 
-        self.empty_state = StateGenerator(0, [0] * len(prices), 0, 0, prices, weights)
-        self.end_state = StateGenerator(2, [0, 0, 1], 25, 3, prices, weights)
-        self.state1 = StateGenerator(1, [0, 1, 0], 10, 2, prices, weights)
+        self.empty_state = StateGenerator(0, [0] * len(prices), prices, weights)  # empty beginning state
+        self.end_state = StateGenerator(2, [0, 0, 1], prices, weights)           # state which can't be developed
+        self.state1 = StateGenerator(1, [0, 1, 0], prices, weights)              # common state with already explored index
 
     def test_get_NextState(self):
 
@@ -17,11 +17,15 @@ class TestStateGenerator(unittest.TestCase):
         self.assertEqual(1, self.empty_state.get_NextState().index)
         self.assertEqual([0, 1, 1], self.state1.get_NextState().taken)
 
+        self.empty_state.get_NextState()                # checking if our start state became unchanged
+        self.assertEqual(0, self.empty_state.index)
+
 
     def test_get_NextState_None(self):
         self.assertEqual(None, self.empty_state.get_NextState().get_NextState().get_NextState().get_NextState())
         self.assertEqual(None, self.state1.get_NextState().get_NextState())
         self.assertEqual(None, self.end_state.get_NextState())
+
 
 if __name__ == "__main__":
     unittest.main()
