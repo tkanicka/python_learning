67fc0e8ca6333571f8b0d30f835b759d670a8643
tkanicka <Slonik7bob@gmail.com>
2019-10-06 21:50:51 +0200

    this solution is actually quite a disgrace, but it can do the job... it needs improvements, a lot of improvements...

--- sudoku/sudoku_solution.py
+++ sudoku/sudoku_solution.py
@@ -1,25 +1,15 @@
-s1 = [[2, 0, 4, 0, 0, 8, 0, 0, 9],
-      [0, 0, 9, 0, 0, 0, 2, 0, 8],
-      [0, 6, 0, 0, 0, 2, 0, 3, 0],
-      [0, 2, 5, 0, 0, 6, 0, 0, 4],
-      [0, 4, 6, 0, 0, 0, 7, 8, 0],
-      [7, 0, 0, 2, 0, 0, 5, 6, 0],
-      [0, 9, 0, 6, 0, 0, 0, 2, 0],
-      [4, 0, 7, 0, 0, 0, 8, 0, 0],
-      [6, 0, 0, 4, 0, 0, 9, 0, 3]]
+import copy
 
+def make_2d(sudoka_list, step):
+    list_2d = []
+    for i in range(0, len(sudoka_list), step):
+        list_2d.append(sudoka_list[i: i + step])
 
-s1s = [[2, 7, 4, 3, 1, 8, 6, 5, 9],
-      [3, 1, 9, 7, 6, 5, 2, 4, 8],
-      [5, 6, 8, 9, 4, 2, 1, 3, 7],
-      [1, 2, 5, 8, 7, 6, 3, 9, 4],
-      [9, 4, 6, 1, 5, 3, 7, 8, 2],
-      [7, 8, 3, 2, 9, 4, 5, 6, 1],
-      [8, 9, 1, 6, 3, 7, 4, 2, 5],
-      [4, 3, 7, 5, 2, 9, 8, 1, 6],
-      [6, 5, 2, 4, 8, 1, 9, 7, 3]]
+    return list_2d
 
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+def print_sudoka(sudoka):
+      for x in sudoka:
+            print(x)
 
 def find_empty(sudoka):
       for x in range(len(sudoka)):
@@ -27,16 +17,19 @@ def find_empty(sudoka):
                   if sudoka[x][y] == 0:
                         return (x,y)
       return None
+
 def check_row(sudoka, position, position_value):
       for i in range(len(sudoka)):
             if sudoka[position[0]][i] == position_value and position[1] != i:
                   return False
       return True
+
 def check_column(sudoka, position,position_value ):
       for i in range(len(sudoka)):
             if sudoka[i][position[1]] == position_value and position[0] != i:
                   return False
       return True
+
 def check_square(sudoka,position, position_value):
       square_position_row = position[0] // 3
       square_position_column = position[1] // 3
@@ -57,37 +50,48 @@ def check_sudoka(sudoka):
 
                         if row == False or column == False or box == False:
                               return False
+      return True
+
+def valid(sudoka,position, position_value):
+      row = check_row(sudoka, position, position_value)
+      column = check_column(sudoka, position, position_value)
+      box = check_square(sudoka, position, position_value)
+
+      if row == False or column == False or box == False:
+            return False
 
       return True
 
 
-def solve_sudoka(sudoka):
-      if find_empty(sudoka) == None and check_sudoka(sudoka) == True:
-            return sudoka
-
-      else:
-            states = [sudoka]
-            while len(states)>0:
-                  current_sudoka = states.pop()
-                  if check_sudoka(current_sudoka) == True:
-
-                        if find_empty(current_sudoka) == None:
+def solve_sudoka1(sudoka):
+      if check_sudoka(sudoka) == True:
+                  states = [sudoka]
+                  while len(states) > 0:
+                        current_sudoka = states.pop()
+                        position = find_empty(current_sudoka)
+                        if position == None:
                               return current_sudoka
+                        for position_value in range(1, 10):
+                              if valid(current_sudoka, position, position_value)== True:
+                                    s = copy.deepcopy(current_sudoka)
+                                    s[position[0]][position[1]] = position_value
+                                    states.append(s)
 
-                        else:
-                              print(current_sudoka)
-                              position = find_empty(current_sudoka)
-                              for position_value in range(10):
-                                    if check_square(current_sudoka,position, position_value) \
-                                            and check_column(current_sudoka,position, position_value)\
-                                            and check_row(current_sudoka,position, position_value) == True:
-                                          current_sudoka[position[0]][position[1]] == position_value
-                                          states.append(current_sudoka)
+file = open("sudoka.txt", "r")
+text = file.read()
+sudoka = []
+numbers = ["0","1","2","3","4","5","6","7","8","9"]
+if len(text) == 81:
+      for x in text:
+            if x in numbers:
+                  sudoka.append(int(x))
 
-                                    current_sudoka[position[0]][position[1]] == 0
-            return False
+sudoka = make_2d(sudoka, 9)
 
-print(solve_sudoka(s1))
+print_sudoka(sudoka)
+print()
+print_sudoka(solve_sudoka1(sudoka))
+
 
 
 
